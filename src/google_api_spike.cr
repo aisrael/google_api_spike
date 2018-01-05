542cc5c1ba516fbe2c321b592b5b25dcae5d2d06
Alistair A. Israel <aisrael@gmail.com>
2018-01-05 17:13:05 +0800

    Now able to request an access token

--- src/google_api_spike.cr
+++ src/google_api_spike.cr
@@ -1,11 +1,23 @@
+require "http"
+
 require "./google_api_spike/*"
 
 # TODO: Write documentation for `GoogleApiSpike`
 module GoogleApiSpike
-
-  HOME_VAR = "HOME"
+  HOME_VAR              = "HOME"
   CREDENTIALS_FILE_NAME = "application_default_credentials.json"
-  WELL_KNOWN_PATH = File.join(".config", "gcloud", CREDENTIALS_FILE_NAME)
+  WELL_KNOWN_PATH       = File.join(".config", "gcloud", CREDENTIALS_FILE_NAME)
+
+  def self.generate_access_token_request(credentials, options = {} of Symbol => String) : Hash(String, String)
+    parameters = {"grant_type" => "refresh_token"}
+    parameters["refresh_token"] = credentials.refresh_token
+    parameters["client_id"] = credentials.client_id
+    parameters["client_secret"] = credentials.client_secret
+    if options.has_key?(:scope)
+      parameters["scope"] = options[:scope]
+    end
+    parameters
+  end
 
   def self.read_json_creds(path)
     UserRefreshCredentials.from_json(File.read(path))
@@ -20,8 +32,24 @@ module GoogleApiSpike
       exit 1
     end
     puts "Reading #{credentials_file_path}..."
+
     credentials = read_json_creds(credentials_file_path)
     p credentials
+
+    request = generate_access_token_request(credentials)
+    p request
+
+    url = UserRefreshCredentials::TOKEN_CREDENTIAL_URI
+    resp = HTTP::Client.post(url, form: request)
+
+    puts resp.status_code
+    unless resp.status_code == 200
+      puts resp.status_message
+      exit 1
+    end
+    json_body = JSON.parse(resp.body)
+
+    puts "Authorization: Bearer #{json_body["access_token"]}"
   end
 end
 
--- src/google_api_spike/user_refresh_credentials.cr
+++ src/google_api_spike/user_refresh_credentials.cr
@@ -1,6 +1,10 @@
 require "json"
 
 class UserRefreshCredentials
+
+  # The default token credential URI
+  TOKEN_CREDENTIAL_URI = "https://www.googleapis.com/oauth2/v4/token"
+
   JSON.mapping(
     client_id: String,
     client_secret: String,
